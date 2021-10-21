4a5693b1641e3ee4f4edb4323b0401cdada606e5
isrhaelbarcelos <isrhael2009@hotmail.com>
2021-10-21 05:03:08 -0300

    Modal e tabela de avaliação do posto de coleta

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,17 @@
+import * as React from 'react';
 import './header.css';
-import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import { green } from '@material-ui/core/colors';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { green} from '@material-ui/core/colors';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-const theme = createTheme({
+  const theme = createTheme({
     palette: {
       primary: {
         main: '#fff',
@@ -21,38 +23,40 @@ const theme = createTheme({
   });
 
 
-export default function Header(){
-    return(
-        
-<header>
-<ThemeProvider theme={theme}>
- <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="primary" elevation={0}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="secondary"
-            aria-label="home"
-            sx={{ mr: 2 }}
-            href="/"
-          >
-            <MenuIcon>
-            </MenuIcon>
-            
-          </IconButton>
-          
-          <Typography>
-            <Button color="secondary" href="/">Home</Button>
-            <Button color="secondary" href="/quemsomos">Quem Somos</Button>
-            <Button color="secondary" href="/postocoleta">Postos de Coleta</Button>
-            <Button color="secondary" href="/noticia">Saiba Mais</Button>
-           </Typography>         
+  export default function Header() {
+    
+    return (
 
-        </Toolbar>
-      </AppBar>
-    </Box>
-    </ThemeProvider>
-    </header>
+      <header>
+        <ThemeProvider theme={theme}>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed" color="primary" elevation={0}>
+              <Toolbar>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="secondary"
+                  aria-label="home"
+                  sx={{ mr: 2 }}
+                  href="/"                
+                >
+                  <MenuIcon>
+                  </MenuIcon>
+
+                </IconButton>
+
+                <Typography>
+                  <Button color="secondary" href="/">Home</Button>
+                  <Button color="secondary" href="/quemsomos">Quem Somos</Button>
+                  <Button color="secondary" href="/postocoleta">Postos de Coleta</Button>
+                  <Button color="secondary" href="/noticia">Saiba Mais</Button>
+                  <Button color="secondary" href="/login" variant="outlined" endIcon={<AccountCircleIcon />}>Login</Button>
+                </Typography>
+
+              </Toolbar>
+            </AppBar>
+          </Box>
+        </ThemeProvider>
+      </header>
     )
-}
+  }
