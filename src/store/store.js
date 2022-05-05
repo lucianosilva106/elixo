572ff6822c5bb695529726a10ef97610b9f9c0a3
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-05-05 10:52:47 -0300

    pagamento via paypal e chat-engine

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware} from 'redux';
-import ReduxThunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducers';
-
-let middleware = [ReduxThunk]
-
-export const store = createStore(rootReducer, applyMiddleware(...middleware))
-
